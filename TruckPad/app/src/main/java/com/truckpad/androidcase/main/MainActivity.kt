da206b0a9a8b0800ac8a11453a26d35c05d20123
caroltr <caroltr.srs@gmail.com>
2020-04-26 19:41:21 -0300

    Hide maps

--- TruckPad/app/src/main/java/com/truckpad/androidcase/main/MainActivity.kt
+++ TruckPad/app/src/main/java/com/truckpad/androidcase/main/MainActivity.kt
@@ -2,44 +2,48 @@ package com.truckpad.androidcase.main
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.truckpad.androidcase.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback, MainContract.View {
+class MainActivity : AppCompatActivity(), MainContract.View { // OnMapReadyCallback
+
+//    private lateinit var mMap: GoogleMap
 
     private lateinit var presenter: MainContract.Presenter
-    private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private var lat: Double = 0.0
+    private var lng: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setPresenter(MainPresenter(this))
 
-        (map as SupportMapFragment).getMapAsync(this)
+//        (map as SupportMapFragment).getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         checkPermissions()
 
-        btn_enter.setOnClickListener {
-            presenter.findRoute(
-                -46.68664,
-                -23.59496,
-                -46.67678,
-                -23.59867,
-                5.0,
-                4.4
-            )
-        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+
+                location?.let {
+                    lat = it.latitude
+                    lng = it.longitude
+                }
+            }
+
+        btn_enter.setOnClickListener {}
     }
 
     /**
@@ -51,26 +55,34 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, MainContract.View 
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
+//    override fun onMapReady(googleMap: GoogleMap) {
+//        mMap = googleMap
+//
+//        // Add a marker in Sydney and move the camera
+//        val currentLocation = LatLng(lat, lng)
+//        mMap.addMarker(MarkerOptions().position(currentLocation))//.title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation))
+//
+//        mMap.animateCamera(CameraUpdateFactory.zoomTo( 17.0f ))
+//    }
 
     override fun setPresenter(presenter: MainContract.Presenter) {
         this.presenter = presenter
     }
 
     private fun checkPermissions() {
-        if (ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.ACCESS_FINE_LOCATION)
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
             != PackageManager.PERMISSION_GRANTED
         ) {
             print("CAROL - check permission: PERMISSION NOT GRANTED")
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this@MainActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
                 // Show an explanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
                 // sees the explanation, try again to request the permission.
@@ -93,13 +105,17 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, MainContract.View 
     }
 
     private fun requestLocationPermission() {
-        ActivityCompat.requestPermissions(this@MainActivity,
+        ActivityCompat.requestPermissions(
+            this@MainActivity,
             arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-            123)
+            123
+        )
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
         when (requestCode) {
             123 -> {
                 // If request is cancelled, the result arrays are empty.
