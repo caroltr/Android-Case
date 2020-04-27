c43a905ad42593275beb8ada6ecefa8d3b9f05a9
caroltr <caroltr.srs@gmail.com>
2020-04-26 22:30:19 -0300

    Organize model classes

---
+++ TruckPad/app/src/main/java/com/truckpad/androidcase/model/Coordinate.kt
@@ -0,0 +1,12 @@
+package com.truckpad.androidcase.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Coordinate(
+    @SerializedName("lat") val latitude: Double,
+    @SerializedName("lng") val longitude: Double
+) {
+    fun toList(): ArrayList<Double> {
+        return arrayListOf(latitude, longitude)
+    }
+}
