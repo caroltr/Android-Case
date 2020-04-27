b794fa818f84434f53ad2d8d220b1ef647c13b31
caroltr <caroltr.srs@gmail.com>
2020-04-27 00:10:21 -0300

    Merge geocode and route requests

--- TruckPad/app/src/main/java/com/truckpad/androidcase/main/MainContract.kt
+++ TruckPad/app/src/main/java/com/truckpad/androidcase/main/MainContract.kt
@@ -7,9 +7,9 @@ import com.truckpad.androidcase.base.BaseView
 interface MainContract {
 
     interface Presenter: BasePresenter {
-        fun findRoute(fromLatitude: Double, fromLongitude: Double,
-                      toLatitude: Double, toLongitude: Double,
-                      fuelConsumption: Double, fuelPrice: Double)
+//        fun findRoute(fromLatitude: Double, fromLongitude: Double,
+//                      toLatitude: Double, toLongitude: Double,
+//                      fuelConsumption: Double, fuelPrice: Double)
 
         fun getGeocode(from: String, to: String)
 
--- TruckPad/app/src/main/java/com/truckpad/androidcase/main/MainPresenter.kt
+++ TruckPad/app/src/main/java/com/truckpad/androidcase/main/MainPresenter.kt
@@ -4,45 +4,61 @@ import android.location.Location
 import android.util.Log
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.truckpad.androidcase.model.*
 import com.truckpad.androidcase.network.ApiFactory
-import com.truckpad.androidcase.model.Coordinate
-import com.truckpad.androidcase.model.Place
-import com.truckpad.androidcase.model.RouteRequest
+import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.BiFunction
+import io.reactivex.functions.Function3
 import io.reactivex.schedulers.Schedulers
+import retrofit2.Response
 import java.net.URLEncoder
 
 class MainPresenter(private val view: MainContract.View) : MainContract.Presenter {
 
-    override fun findRoute(
-        fromLatitude: Double,
-        fromLongitude: Double,
-        toLatitude: Double,
-        toLongitude: Double,
+    private lateinit var fromCoordinate: Coordinate
+    private lateinit var toCoordinate: Coordinate
+
+    fun findRoute(
+        fromCoordinate: Coordinate,
+        toCoordinate: Coordinate,
         fuelConsumption: Double,
         fuelPrice: Double
-    ) {
-        val fromLocation = Coordinate(fromLatitude, fromLongitude)
-        val toLocation = Coordinate(toLatitude, toLongitude)
+    ): Observable<Response<RouteResponse>> {
 
-        val from = Place(fromLocation.toList())
-        val to = Place(toLocation.toList())
+        val from = Place(fromCoordinate.toList())
+        val to = Place(toCoordinate.toList())
         val route = RouteRequest(fuelConsumption, fuelPrice, arrayListOf(from, to))
 
-        val r = ApiFactory.geoApi
+        return ApiFactory.geoApi
             .findRoute(route)
+            .toObservable()
+    }
+
+    override fun getGeocode(from: String, to: String) {
+
+        val fromObs = fetchGeocode(from)
+        val toObs = fetchGeocode(to)
+        val result = Observable.zip(fromObs, toObs,
+            BiFunction<Coordinate?, Coordinate?, List<Coordinate>> { fromCoord, toCoord -> listOf(fromCoord, toCoord) })
+            .flatMap {
+                findRoute(it[0], it[1], 5.0, 4.4)
+            }
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({ response ->
                 Log.e("Carol", "Success")
+
+                if (response.isSuccessful) {
+                    response.body()?.let {
+
+                    }
+                }
+
             }, {
                 Log.e("Carol", "Error")
             })
-    }
-
-    override fun getGeocode(from: String, to: String) {
-        fetchGeocode(from)
-        fetchGeocode(to)
     }
 
     override fun getLastLocation(fusedLocationClient: FusedLocationProviderClient) {
@@ -52,22 +68,16 @@ class MainPresenter(private val view: MainContract.View) : MainContract.Presente
             }
     }
 
-    private fun fetchGeocode(address: String) {
+    private fun fetchGeocode(address: String): Observable<Coordinate?> {
         val googleKey = "AIzaSyBW2GhHdV3obS6jEkTGHjod5REtuSSKFcQ"
-        val addressEncoded = URLEncoder. encode(address,"UTF-8");
+        val addressEncoded = URLEncoder.encode(address, "UTF-8")
 
-        val result1 = ApiFactory.geocodeApi
+        return ApiFactory.geocodeApi
             .getGeocode(addressEncoded, googleKey)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ response ->
-                response.body()?.let {
+            .map { res ->
+                res.body()?.let {
                     it.results.first().geometry.location
                 }
-
-                Log.e("Carol", "Success")
-            }, {
-                Log.e("Carol", "Error")
-            })
+            }.toObservable()
     }
 }
