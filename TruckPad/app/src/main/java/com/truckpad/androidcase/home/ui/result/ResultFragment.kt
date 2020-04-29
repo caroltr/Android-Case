fd5946722c83e86c4fb763dd3e56544aec4767fd
caroltr <caroltr.srs@gmail.com>
2020-04-29 14:00:18 -0300

    Improve architecture on result view

--- TruckPad/app/src/main/java/com/truckpad/androidcase/home/ui/result/ResultFragment.kt
+++ TruckPad/app/src/main/java/com/truckpad/androidcase/home/ui/result/ResultFragment.kt
@@ -5,71 +5,62 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.truckpad.androidcase.R
 import com.truckpad.androidcase.model.Coordinate
 import com.truckpad.androidcase.model.ResultData
 import com.truckpad.androidcase.util.Extra
-import kotlinx.android.synthetic.main.fragment_result.view.*
+import kotlinx.android.synthetic.main.fragment_result.*
 import kotlinx.android.synthetic.main.fragment_result.view.btn_map
-
-import java.util.ArrayList
 
 class ResultFragment : Fragment() {
 
-    private lateinit var homeViewModel: ResultViewModel
+    private lateinit var resultViewModel: ResultViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
+        resultViewModel =
             ViewModelProviders.of(this).get(ResultViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_result, container, false)
 
-        var route: List<List<Double>>? = null
-        arguments?.get(Extra.PRICE.value)?.let {
-            val result = it as ResultData
+        resultViewModel.handleData(arguments)
 
-            route = result.routeResult.route
+        resultViewModel.result.observe(viewLifecycleOwner, Observer { displayData(it) })
+        resultViewModel.route.observe(viewLifecycleOwner, Observer {  })
 
-            root.tv_from.text = result.from
-            root.tv_to.text = result.to
-            root.tv_distance.text = result.routeResult.distance
-            root.tv_duration.text = result.routeResult.duration
-            root.tv_toll.text = result.routeResult.tollCost
-            root.tv_amount_fuel.text = result.routeResult.fuelUsage
-            root.tv_price_fuel.text = result.routeResult.fuelCost
-            root.tv_total_price.text = ""
-
-            // Prices
-            root.tv_general.text = result.priceResponse.geral.toString()
-            root.tv_granel.text = result.priceResponse.granel.toString()
-            root.tv_neogranel.text = result.priceResponse.neogranel.toString()
-            root.tv_frigorificada.text = result.priceResponse.frigorificada.toString()
-            root.tv_dangerous.text = result.priceResponse.perigosa.toString()
+        root.btn_map.setOnClickListener {
+//            showMapFragment(route)
         }
-
-        root.btn_map.setOnClickListener { showMapFragment(route) }
 
         return root
     }
 
-    private fun showMapFragment(route: List<List<Double>>?) {
+    private fun displayData(result: ResultData) {
+        tv_from.text = result.from
+        tv_to.text = result.to
+        tv_distance.text = result.routeResult.distance
+        tv_duration.text = result.routeResult.duration
+        tv_toll.text = result.routeResult.tollCost
+        tv_amount_fuel.text = result.routeResult.fuelUsage
+        tv_price_fuel.text = result.routeResult.fuelCost
+        tv_total_price.text = ""
 
-        val r = ArrayList<Coordinate>()
-        route?.forEach { list ->
-            val coord = Coordinate(list[0], list[1])
-            r.add(coord)
-        }
+        // Prices
+        tv_general.text = result.priceResponse.geral.toString()
+        tv_granel.text = result.priceResponse.granel.toString()
+        tv_neogranel.text = result.priceResponse.neogranel.toString()
+        tv_frigorificada.text = result.priceResponse.frigorificada.toString()
+        tv_dangerous.text = result.priceResponse.perigosa.toString()
+    }
 
+    private fun showMapFragment(route: ArrayList<Coordinate>) {
         val bundle = Bundle()
-        bundle.putParcelableArrayList(Extra.ROUTE.value, r)
-
-
-
+        bundle.putParcelableArrayList(Extra.ROUTE.value, route)
         findNavController().navigate(R.id.action_navigation_result_to_navigation_map, bundle)
     }
 }
--- TruckPad/app/src/main/java/com/truckpad/androidcase/home/ui/result/ResultViewModel.kt
+++ TruckPad/app/src/main/java/com/truckpad/androidcase/home/ui/result/ResultViewModel.kt
@@ -1,13 +1,38 @@
 package com.truckpad.androidcase.home.ui.result
 
+import android.os.Bundle
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.truckpad.androidcase.model.Coordinate
+import com.truckpad.androidcase.model.ResultData
+import com.truckpad.androidcase.util.Extra
+import java.util.ArrayList
 
 class ResultViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _result = MutableLiveData<ResultData>()
+    private val _route = MutableLiveData<ArrayList<Coordinate>>()
+
+    val result: LiveData<ResultData> = _result
+    val route: LiveData<ArrayList<Coordinate>> = _route
+
+    fun handleData(arguments: Bundle?) {
+        arguments?.get(Extra.PRICE.value)?.let {
+            (it as? ResultData)?.let { result ->
+                val route = parseRoute(result.routeResult.route)
+
+                _result.value = result
+                _route.value = route
+            }
+        }
     }
-    val text: LiveData<String> = _text
+
+    private fun parseRoute(rawRoute: List<List<Double>>): ArrayList<Coordinate> {
+        val route = ArrayList<Coordinate>()
+        rawRoute.forEach { list ->
+            route.add(Coordinate(list[0], list[1]))
+        }
+        return route
+    }
 }
