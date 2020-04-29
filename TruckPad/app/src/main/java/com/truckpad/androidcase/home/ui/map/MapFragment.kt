4b88d94a98304726197ce556cbbb6c071dfe6b92
caroltr <caroltr.srs@gmail.com>
2020-04-29 08:46:42 -0300

    Show map on fragment

--- TruckPad/app/src/main/java/com/truckpad/androidcase/home/ui/map/MapFragment.kt
+++ TruckPad/app/src/main/java/com/truckpad/androidcase/home/ui/map/MapFragment.kt
@@ -1,15 +1,22 @@
 package com.truckpad.androidcase.home.ui.map
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.truckpad.androidcase.R
+import kotlinx.android.synthetic.main.fragment_map.view.*
 
-class MapFragment : Fragment() {
+
+class MapFragment : Fragment(), OnMapReadyCallback {
+
     private lateinit var mapViewModel: MapViewModel
+    private lateinit var mapView: MapView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +27,44 @@ class MapFragment : Fragment() {
             ViewModelProviders.of(this).get(MapViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_map, container, false)
 
+        mapView = root.map
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
         return root
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+//        googleMap.isMyLocationEnabled = true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onPause() {
+        mapView.onPause()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        mapView.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
